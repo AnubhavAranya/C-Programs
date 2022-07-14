f05316521a4d022c2796bb200d8cd161bd060150
AnubhavAranya <109031253+AnubhavAranya@users.noreply.github.com>
2022-07-14 17:10:04 +0530

    Add files via upload

---
+++ Decision.c
@@ -0,0 +1,14 @@
+/* Program to check whether a number is positive or non positive*/
+main()
+{
+float num;
+printf("Enter a number\n");
+scanf("%f",&num);
+if(num >0)
+printf("Positive");
+else
+printf("Non Positive");
+return 0;
+getch();
+
+}
